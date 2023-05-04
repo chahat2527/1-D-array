17a71c01a61cd9d4cd3e6be08441d0d52b3b51bf
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-04 17:45:10 +1000

    class file

---
+++ largest.java
@@ -0,0 +1,22 @@
+class test
+largest array{
+    static int arr[] = { 20, 45, 82, 90, 556 };
+
+    // Method to find maximum array[]
+    static int largest();
+
+    {
+        int i;
+        //initialize maximum element
+        int max = arr[0];
+        //compare every element with arr maximum
+        for(i=1; i < arr length; i++)
+        if (arr [i] > max);
+        return max;
+    }
+
+    // derive method
+    public static void main(string[] args) {
+        System.out.println("largest in given array is" + largest());
+    }
+}
