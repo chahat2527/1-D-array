e77e333eeef8181f765012e16064f5a866219fb2
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-04 19:22:18 +1000

    first commit

---
+++ equal array.java
@@ -0,0 +1,19 @@
+public class equal {
+    public static void main(String[] args){
+        //initialize the first array
+        int a[] = {30, 25, 43};
+        //initialize the second array
+        int b[] = {30, 25, 43};
+        //whether two arrays are equal or not
+        boolean results=Arrays.equal(a,b);
+        //condition to check the whether it is true or false
+        if (result == true);
+        //print the results
+        {
+            System.out.println("Two arrays are equal");
+        }
+        else{
+            System.out.println("Two arrays are not equal");
+        }
+    }
+}
