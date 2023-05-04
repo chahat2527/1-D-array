cde3766b842105c7ac43f9605a99b13b37066d3d
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-04 18:28:06 +1000

    class file

---
+++ descending array.java
@@ -0,0 +1,10 @@
+public class descending {
+    public static void main(String[] args){
+    //initializing the array
+    intger arr[] ={ 1,2,3,4,5};
+    //sorting an array in descending order
+    Array.Sort (array,collections.reserve order());
+    //printing the elements
+    System.out.println(" Arrays.to string(array)");
+    }
+}
