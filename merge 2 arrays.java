0fae17ea694b8f0dc7387decfe52ad6036e26651
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-04 18:53:09 +1000

    class file

---
+++ merge 2 arrays.java
@@ -0,0 +1,21 @@
+public class merge {
+    public static void main(String[] args){
+        //first array
+        int[] a= {10,20,30,40,50};
+        //second array
+        int[] b= {60,70,80,90,100};
+        //determine the first array
+        int a1=a.length
+        //determine the second array
+        int b1=a.length
+        //initializing the array
+        int c1=a1+b1;
+        //create the resultant
+        int[]c=new int [c1];
+        //user the pre define array
+        System.out.println(a,0,c,0,a1);
+        System.out.println(b,0,c,a1, b1);
+        //Resultant array
+        System.out.println("Arrays to string(c)");
+    }
+}
