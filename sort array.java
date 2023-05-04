136bad7aeea3cee00effe68166d04d6ad73f5cc8
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-04 18:14:35 +1000

    class file

---
+++ sort array.java
@@ -0,0 +1,9 @@
+public class sort {
+    public static void main(String[] args){
+        int[] arr= {13,7,6,14,101,102};
+        //Applying sort() method
+        Arrays.sort(arr);
+        //printing the array after sorting
+        system.out.println("Modified array[]:" + Arrays to striing(arr));
+    }
+}
